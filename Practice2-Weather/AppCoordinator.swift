61fdd461ae59146a84f8b1c7c7f08fb5258c70dd
Kristina <perekrist31@gmail.com>
2020-08-28 16:45:07 +0700

    AppCoordinator

--- Practice2-Weather/AppCoordinator.swift
+++ Practice2-Weather/AppCoordinator.swift
@@ -10,22 +10,16 @@ import UIKit
 
 class AppCoordinator: Coordinator {
     // MARK: - Properties
-    let window: UIWindow?
+    let window: UIWindow
     
-    let rootViewController: UINavigationController = {
-        return UINavigationController(rootViewController: UIViewController())
-    }()
+    let rootViewController: UINavigationController = UINavigationController()
     
     // MARK: - Coordinator
-    init(window: UIWindow?) {
+    init(window: UIWindow) {
         self.window = window
     }
     
     override func start() {
-        guard let window = window else {
-            return
-        }
-        
         let mapCoordinator = MapCoordinator(rootViewController: rootViewController)
         self.addChildCoordinator(mapCoordinator)
         mapCoordinator.start()
